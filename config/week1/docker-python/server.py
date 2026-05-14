73403e006026ea165c54ebfe233f07b732ebf2b9
elyaskc <elyas8e@gmail.com>
2026-05-14 17:53:41 +0200

    Update server.py

--- config/week1/docker-python/server.py
+++ config/week1/docker-python/server.py
@@ -1,7 +1,12 @@
-def main():
-    print("Hello from Python container")
-    while True:
-    	pass
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from Python container")
 
 if __name__ == "__main__":
-    main()
+    server = HTTPServer(("0.0.0.0", 5000), Handler)
+    print("Server running on port 5000")
+    server.serve_forever()
